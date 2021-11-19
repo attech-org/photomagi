1894baae60a85ff3e537d20d4c1fdbd6d2bf6065
alexandrtovmach <alexandrtovmach@gmail.com>
2021-11-19 18:00:43 +0200

    feat: Added logic for selecting fighters

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { Button, Card, Divider, Image, Layout, Space } from 'antd';
-import { useSelector } from 'react-redux';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled, { css } from 'styled-components';
 
-// import { decreaseCounter, increaseCounter, loadFighters } from './redux/app/actions';
+import { loadFighters } from './redux/app/actions';
 import { AppStore } from './redux/app/reducer';
 import { RootStore } from './redux/store';
 
@@ -30,6 +31,15 @@ const FighterCard = styled(Card)`
   margin: 1rem;
   background-color: rgba(238, 130, 238, 0.4);
   width: 200px;
+  box-shadow: ${(props) => {
+    if (props['aria-colindex'] === 0) {
+      return '0 0 0 5px blue';
+    }
+    if (props['aria-colindex'] === 1) {
+      return '0 0 0 5px green';
+    }
+    return 'none';
+  }};
 `;
 
 const FighterCardMeta = styled(Card.Meta)``;
@@ -42,6 +52,7 @@ const StyledFighterImage = styled(Image)`
 
 const FightersPanel = styled(Space)`
   width: 100%;
+  min-height: 200px;
   overflow-x: auto;
   background-color: wheat;
 `;
@@ -56,56 +67,53 @@ const ControlPanel = styled(Space)`
 `;
 
 const App: React.FunctionComponent = () => {
-  // const dispatch = useDispatch();
-  // const count = useSelector<RootStore, AppStore['counter']>((store) => store.app.counter);
+  const dispatch = useDispatch();
   const fighters = useSelector<RootStore, AppStore['fighters']>((store) => store.app.fighters);
+  const [selectedFighters, onSelectedFightersChange] = useState<string[]>([]);
 
-  // const handleInc = () => {
-  //   dispatch(increaseCounter(1));
-  // };
+  const fetchUsers = async () => {
+    dispatch(loadFighters());
+  };
 
-  // const handleDec = () => {
-  //   dispatch(decreaseCounter(1));
-  // };
-
-  // const fetchUsers = async () => {
-  //   dispatch(loadFighters());
-  // };
+  const handleFighterClick = (fighterName: string) => {
+    if (selectedFighters.length < 2) {
+      onSelectedFightersChange([...selectedFighters, fighterName]);
+    }
+    if (selectedFighters.includes(fighterName)) {
+      onSelectedFightersChange(selectedFighters.filter((name) => name !== fighterName));
+    }
+  };
 
   return (
     <Wrapper>
       <Layout.Header>Header</Layout.Header>
       <Content>
-        {/* <Button onClick={handleInc} shape="circle">
-          +
-        </Button>
-        {count}
-        <Button onClick={handleDec} shape="circle">
-          -
-        </Button>
-        <Button onClick={fetchUsers}>Get All Users</Button> */}
         <FightersPanel>
-          {fighters.map(({ name, source, attack, defense, health }) => (
-            <FighterCard
-              key={name}
-              hoverable
-              cover={<StyledFighterImage alt="example" src={source} />}
-            >
-              <FighterCardMeta
-                title={name}
-                description={`Attack: ${attack} Defence: ${defense} Health: ${health}`}
-              />
-            </FighterCard>
-          ))}
+          {fighters.length ? (
+            fighters.map(({ name, source, attack, defense, health }) => (
+              <FighterCard
+                key={name}
+                aria-colindex={selectedFighters.indexOf(name)}
+                onClick={() => handleFighterClick(name)}
+                cover={<StyledFighterImage alt="example" src={source} />}
+              >
+                <FighterCardMeta
+                  title={name}
+                  description={`Attack: ${attack} Defence: ${defense} Health: ${health}`}
+                />
+              </FighterCard>
+            ))
+          ) : (
+            <Button onClick={fetchUsers}>Load fighters</Button>
+          )}
         </FightersPanel>
         <Divider />
         <ControlPanel>
-          <Button type="primary" danger size="large">
+          <Button disabled={selectedFighters.length < 2} type="primary" danger size="large">
             Fight!
           </Button>
         </ControlPanel>
       </Content>
-      <Layout.Footer>Footer</Layout.Footer>
     </Wrapper>
   );
 };
